2bb5c56a6496ac95ecd324050c10a6ccd5864b04
ironhandedlayman <leadhyena@gmail.com>
2025-03-08 07:57:36 -0500

    moved checkpoint, extra git settings

--- ironhandedlayman-home.nix
+++ ironhandedlayman-home.nix
@@ -35,7 +35,7 @@ in {
   
     sessionVariables = {
       EDITOR = lib.mkForce "nvim";
-      DEFAULT_BROWSER = "${pkgs.firefox}/bin/firefox";
+      DEFAULT_BROWSER = "${pkgs.firefox}/bin/firefox"; # NOTE: think about other browsers given events around 27-Feb-25
     };
   };
 
@@ -43,7 +43,29 @@ in {
     enable = true;
     userName = "ironhandedlayman";
     userEmail = "leadhyena@gmail.com";
+    aliases = {
+      c = "commit --no-verify -a";
+      adog = "log --all --decorate --oneline --graph";
+    };
     extraConfig = {
+      branch.sort = "committerdate";
+      tag.sort = "version:refname";
+      column.ui="auto";
+      color.ui="true";
+      commit.verbose = true;
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      lfs = {
+        enable = true;
+        skipSmudge = true;
+      };
+      pull.rebase = false; # NOTE: revisit this stance
+      merge.tool = "meld";
+      help.autocorrect = "prompt";
       init.defaultBranch = "main"; 
     };
   };
