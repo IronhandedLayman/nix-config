66d10540fe245795f5ec9c45c33ef7f8a439c0ad
ironhandedlayman <leadhyena@gmail.com>
2025-07-05 09:51:54 -0400

    readded btop, cleaned up systemwide packages

--- kataribe-configuration.nix
+++ kataribe-configuration.nix
@@ -5,12 +5,12 @@
   nixpkgs.config.allowBroken = true;
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  environment.systemPackages =
-    [
-      pkgs.alacritty
-      pkgs.wezterm
-      pkgs.vim
-      pkgs.neovim
+  environment.systemPackages = with pkgs; [
+      alacritty
+      wezterm
+      vim
+      neovim
+      btop
     ];
 
   nix = {
